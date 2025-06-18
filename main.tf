c722dea0e550f573ec1c5eb688a754a010c3dd07
kaizenacademy <kaizen.techacademy@gmail.com>
2025-06-18 00:49:35 +0000

    added bucket

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_instance" "web" {
   ami           = "ami-09e6f87a47903347c"
   instance_type = "t2.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "kaizen-adileta"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
