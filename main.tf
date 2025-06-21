066179da8809fe250ef67fdb6a2d0c2bc617e797
kaizenacademy <126742188+kaizenacademy@users.noreply.github.com>
2025-06-21 10:36:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,13 @@
 provider aws {
-    region = var.region
+    region = "us-east-2"
 }
 
 resource "aws_instance" "web" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
+  ami           = "ami-06971c49acd687c30"
+  instance_type = "t2.micro"
 }
 
-variable region {}
-variable ami_id {}
-variable instance_type {}
+#variable region {}
+#variable ami_id {}
+#variable instance_type {}
 
-terraform {
-  backend "s3" {
-    bucket = "kaizen-adileta"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    use_lockfile = true
-  }
-}
